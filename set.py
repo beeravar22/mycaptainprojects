2bbc1615c4d523999932ae8fb2a6035fa81c4197
beeravar22 <109066422+beeravar22@users.noreply.github.com>
2022-10-18 22:45:16 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,13 @@
+from re import S
+
+
+E={1,2,3,4,5}
+N={0,2,4,6,8}
+S_union=E|N
+print ("union of E and N is", S_union )
+S_intersection=E & N
+print( "intersection of E and N is",S_intersection )
+S_difference=N-E
+print("difference of E and N is",S_difference)
+S_symmetric_difference=E^N
+print("symmetric difference of E and N is",S_symmetric_difference)
